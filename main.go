e2d6dc30f15da0dff04328afd941140fd6b2afa1
k3rnel-pan1c <60621392+k3rnel-pan1c@users.noreply.github.com>
2026-05-04 16:14:01 +0200

    chapter 3 implementation

--- main.go
+++ main.go
@@ -2,51 +2,293 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
-	"unicode"
 
+	"golang.org/x/sys/unix"
 	"golang.org/x/term"
 )
 
-/* terminal */
+/* defines/macros/functions */
 
-func enableRawMode() (*term.State, error) {
-	return term.MakeRaw(int(os.Stdin.Fd()))
+var version = "0.0.1"
+
+func ctrlKey(k int) int {
+	return k & 0x1f
 }
 
-func disableRawMode(oldState *term.State) {
-	err := term.Restore(int(os.Stdin.Fd()), oldState)
+const (
+	ARROW_LEFT = 1000 + iota
+	ARROW_RIGHT
+	ARROW_UP
+	ARROW_DOWN
+	DEL_KEY
+	HOME_KEY
+	END_KEY
+	PAGE_UP
+	PAGE_DOWN
+)
+
+/* data */
+
+type editorConfig struct {
+	cx, cy     int
+	screenrows int
+	screencols int
+	termios    *term.State
+}
+
+var E editorConfig
+
+/* terminal */
+
+func die(err error) {
+	// clear screen
+	os.Stdout.Write([]byte("\x1b[2J"))
+	// move cursor to top left
+	os.Stdout.Write([]byte("\x1b[H"))
+
+	panic(err)
+}
+
+func enableRawMode() {
+	var err error
+	E.termios, err = term.MakeRaw(int(os.Stdin.Fd()))
 	if err != nil {
-		panic(err)
+		die(err)
 	}
+	termios, err := unix.IoctlGetTermios(int(os.Stdin.Fd()), unix.TCGETS)
+	if err != nil {
+		die(err)
+	}
+	termios.Cc[unix.VMIN] = 0
+	termios.Cc[unix.VTIME] = 1
+	unix.IoctlSetTermios(int(os.Stdin.Fd()), unix.TCSETS, termios)
+
+}
+
+func disableRawMode() {
+	err := term.Restore(int(os.Stdin.Fd()), E.termios)
+	if err != nil {
+		die(err)
+	}
+}
+
+func editorReadKey() int {
+	buf := make([]byte, 1)
+	for {
+		n, err := os.Stdin.Read(buf)
+		if err != nil && err != io.EOF {
+			die(err)
+		}
+		if n == 1 {
+			break
+		}
+	}
+
+	c := buf[0]
+
+	if c == '\x1b' {
+		seq0 := make([]byte, 1)
+		seq1 := make([]byte, 1)
+		seq2 := make([]byte, 1)
+
+		n, _ := os.Stdin.Read(seq0)
+		if n != 1 {
+			return '\x1b'
+		}
+
+		n, _ = os.Stdin.Read(seq1)
+		if n != 1 {
+			return '\x1b'
+		}
+
+		if seq0[0] == '[' {
+
+			if seq1[0] >= '0' && seq1[0] <= '9' {
+				n, _ = os.Stdin.Read(seq2)
+				if n != 1 {
+					return '\x1b'
+				}
+
+				if seq2[0] == '~' {
+					switch seq1[0] {
+					case '1':
+						return HOME_KEY
+					case '3':
+						return DEL_KEY
+					case '4':
+						return END_KEY
+					case '5':
+						return PAGE_UP
+					case '6':
+						return PAGE_DOWN
+					case '7':
+						return HOME_KEY
+					case '8':
+						return END_KEY
+					}
+				}
+			} else {
+				switch seq1[0] {
+				case 'A':
+					return ARROW_UP
+				case 'B':
+					return ARROW_DOWN
+				case 'C':
+					return ARROW_RIGHT
+				case 'D':
+					return ARROW_LEFT
+				case 'H':
+					return HOME_KEY
+				case 'F':
+					return END_KEY
+				}
+			}
+		} else if seq0[0] == 'O' {
+			switch seq1[0] {
+			case 'H':
+				return HOME_KEY
+			case 'F':
+				return END_KEY
+			}
+		}
+		return '\x1b'
+	} else {
+		return int(c)
+	}
+}
+
+func getWindowSize() (int, int) {
+	cols, rows, err := term.GetSize(int(os.Stdin.Fd()))
+	if err != nil {
+		die(err)
+	}
+	return rows, cols
+}
+
+/* output */
+
+func editorDrawRows(ab []byte) []byte {
+	for y := range E.screenrows {
+		if y == E.screenrows/3 {
+			welcome := fmt.Sprintf("Kilo editor -- version %s", version)
+			if len(welcome) > E.screencols {
+				welcome = welcome[:E.screencols]
+			}
+			padding := (E.screencols - len(welcome)) / 2
+			ab = append(ab, '~')
+			padding--
+			for range padding {
+				ab = append(ab, ' ')
+			}
+			ab = append(ab, []byte(welcome)...)
+		} else {
+			ab = append(ab, '~')
+		}
+
+		// clear line
+		ab = append(ab, []byte("\x1b[K")...)
+		if y < E.screenrows-1 {
+			ab = append(ab, []byte("\r\n")...)
+		}
+	}
+
+	return ab
+}
+
+func editorRefreshScreen() {
+	ab := make([]byte, 0)
+	// hide cursor
+	ab = append(ab, []byte("\x1b[?25l")...)
+	// move cursor to top left
+	ab = append(ab, []byte("\x1b[H")...)
+
+	ab = editorDrawRows(ab)
+
+	// move cursor to coordinates
+	buf := fmt.Sprintf("\x1b[%d;%dH", E.cy+1, E.cx+1)
+	ab = append(ab, []byte(buf)...)
+
+	// show cursor
+	ab = append(ab, []byte("\x1b[?25h")...)
+
+	os.Stdout.Write(ab)
+}
+
+/* input */
+
+func editorMoveCursor(key int) {
+	switch key {
+	case ARROW_LEFT:
+		if E.cx != 0 {
+			E.cx--
+		}
+	case ARROW_RIGHT:
+		if E.cx != E.screencols-1 {
+			E.cx++
+		}
+	case ARROW_UP:
+		if E.cy != 0 {
+			E.cy--
+		}
+	case ARROW_DOWN:
+		if E.cy != E.screenrows-1 {
+			E.cy++
+		}
+	}
+}
+
+func editorProcessKeypress() bool {
+	c := editorReadKey()
+
+	switch c {
+	case ctrlKey('q'):
+		// clear screen
+		os.Stdout.Write([]byte("\x1b[2J"))
+		// move cursor to top left
+		os.Stdout.Write([]byte("\x1b[H"))
+		return false
+	case HOME_KEY:
+		E.cx = 0
+	case END_KEY:
+		E.cx = E.screencols - 1
+	case PAGE_UP:
+		for range E.screenrows {
+			editorMoveCursor(ARROW_UP)
+		}
+	case PAGE_DOWN:
+		for range E.screenrows {
+			editorMoveCursor(ARROW_DOWN)
+		}
+	case ARROW_UP:
+		editorMoveCursor(c)
+	case ARROW_LEFT:
+		editorMoveCursor(c)
+	case ARROW_DOWN:
+		editorMoveCursor(c)
+	case ARROW_RIGHT:
+		editorMoveCursor(c)
+	}
+	return true
 }
 
 /* init */
 
-func main() {
-	oldState, err := enableRawMode()
-	if err != nil {
-		panic(err)
-	}
-	defer disableRawMode(oldState)
+func initEditor() {
+	E.cx = 0
+	E.cy = 0
+	E.screenrows, E.screencols = getWindowSize()
+}
 
-	buf := make([]byte, 1)
+func main() {
+	enableRawMode()
+	defer disableRawMode()
+	initEditor()
 
 	for {
-		_, err := os.Stdin.Read(buf)
-		if err != nil {
-			panic(err)
-		}
-
-		c := buf[0]
-
-		if unicode.IsControl(rune(c)) {
-			fmt.Printf("%d\r\n", c)
-		} else {
-			fmt.Printf("%d ('%c')\r\n", c, c)
-		}
-
-		if c == 'q' {
+		editorRefreshScreen()
+		if !editorProcessKeypress() {
 			break
 		}
 	}
